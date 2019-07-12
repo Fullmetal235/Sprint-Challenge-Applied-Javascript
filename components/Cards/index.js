e6959522b80e50bcd20cf273e18d00d1b07d1f64
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-07-12 13:06:51 -0500

    Creating function for cards.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,35 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get(` https://lambda-times-backend.herokuapp.com/articles`)
+.then(response => {
+    for (let i = 0; i < response.data.articles.length; i++) {
+        //creating elements
+      const card = document.createElement('div');
+      const headline = document.createElement('div');
+      const author = document.createElement('div')
+      const img = document.createElement('img')
+      const span = document.createElement('span')
+      //creating class
+      card.classList.add('card')
+      headline.classList.add('headline')
+      author.classList.add('author')
+      //setting content
+      card.textContent = response.data.articles[i]
+      document.querySelector('.cards-container').appendChild(card)
+      //returning HTML
+      card.innerHTML = `<div class="card">
+         <div class="headline">${headline}</div>
+         <div class="author">
+          <div class="img-container">
+             <img src=${authorPhoto} />
+           </div>
+           <span>By ${authorName}</span>
+         </div>
+       </div>`
+       return card;
+    }
+  })
+  .catch(error => {
+    console.log("Error: ", err)
+  })
